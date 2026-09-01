b7853af40a0fa53b6d323e27f437ad63096d0a94
Abhishek_Malla <abhishekkodavanji@gmail.com>
2026-09-01 12:23:14 +0530

    Add updated settings.py

--- config/settings.py
+++ config/settings.py
@@ -28,10 +28,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = [
+    host.strip()
+    for host in os.getenv(
+        "ALLOWED_HOSTS",
+        "127.0.0.1,localhost"
+    ).split(",")
+    if host.strip()
+]
 
 # Application definition
 
